bd54f5531db1041af93169cd9443195981e00354
TakumiOda <xiaotiantakumi@gmail.com>
2023-02-18 08:59:08 +0900

    PyCharmでのデバッグ構成

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import uvicorn
 from pydantic import BaseModel
 from fastapi import FastAPI
 
@@ -10,4 +11,9 @@ class HelloWorld(BaseModel):
 
 @app.get("/hello-world", response_model=HelloWorld)
 async def hello_world():
-    return HelloWorld(message="Hello world5")
+    return HelloWorld(message="Hello world4")
+
+# デバッグするために以下が必要となります。
+# 本番環境では、DockerFileより生成されるのでsrc.api.mainが__main__になるため以下は実行されません。
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True, workers=1)
